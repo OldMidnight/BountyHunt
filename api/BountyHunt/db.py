3b3807a07fe5f88ac5b68c3e215c74b853fd32df
OldMidnight <fareedidris20@gmail.com>
2021-09-19 12:56:48 +0100

    trigger webhook 5

--- api/BountyHunt/db.py
+++ api/BountyHunt/db.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from flask import current_app
 import os
 
 engine = create_engine(os.environ['BH_DATABASE_URI'], convert_unicode=True)
@@ -10,5 +9,4 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-  import API.models
   Base.metadata.create_all(bind=engine)
